5cd2ce940a6a5b2e15e6937372ccfc8527c92b54
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-10-04 23:13:15 +0530

    Hotel booking system using Java Swqing and MySQL database

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_booking"; // replace with your DB URL
     private static final String USER = "root"; // replace with your DB user
-    private static final String PASSWORD = "Jayesh@123"; // replace with your DB password
+    private static final String PASSWORD = "Akg@845307"; // replace with your DB password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
